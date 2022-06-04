19b15f7cc00bfe72b350c90244c7067ed29ada32
Tkamunya1 <titus.kamunya@student.moringaschool.com>
2022-06-04 10:18:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(newArray, event){
+    let thankYouCards=[]
+    for(let i=0; i < newArray.length; i++){
+        thankYouCards.push(`Thank you, ${newArray[i]}, for the wonderful ${event} gift!` )
+}
+     return thankYouCards
+}
+function countDown(number){
+    while(number>0){
+        console.log(number);
+        number-=1;
+    }
+    console.log(number)
+}
